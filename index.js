27098c06936ee055d1d8a01b4c2984c8dc2a9ce5
João Vitor Alves Muniz <joao.vitor@akamaetech.com>
2024-02-08 14:49:52 -0300

    aplicando a busca de cep

--- index.js
+++ index.js
@@ -3,26 +3,4 @@
 
 
 
-async function searchCep(cep) {
-    let url = `https://viacep.com.br/ws/${cep}/json/`;
-    console.log(url);
-
-    const options = {
-        method: 'GET' 
-    };
-
-    try {
-        const response = await fetch(url, options);
-        if (response.ok) {
-            const data = await response.json();
-            vue.contentCep = data
-            return data; 
-        } else {
-            console.error('Error na procura:', response.status);
-        }
-    } catch (error) {
-        console.error('Error na requisição:', error);
-    }
-}
-
 
